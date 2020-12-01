7f542f9951ceecd0a1126c753aebeda35b3eaa27
zhangfan <zf2016@mail.ustc.edu.cn>
2020-12-01 09:39:10 +0800

    update version-2020.12.01

--- experiments/rib_centerline_seg/data_process/3_convert_deploy_model.py
+++ experiments/rib_centerline_seg/data_process/3_convert_deploy_model.py
@@ -1,27 +1,33 @@
-
-""" Convert torch model in pytorch to torchscript model in libtorch.
+""" Convert pytorch model to torchscript model.
 """
-import os
 import sys
-import torch
 sys.path.append("/fileser/zhangfan/LungProject/lung_segment/")
+import os
+import torch
 
 from network.deploy_unet_block3 import UNet
-from model_utils.convert_model_to_torchscript import load_model_weights, convert_model_to_torchscript
+os.environ['CUDA_VISIBLE_DEVICES'] = "4"
 
+pytorch_model_dir = "/fileser/zhangfan/LungProject/lung_segment/experiments/rib_centerline_seg/rib_centerline_fine_seg/" \
+                    "output/rib_centerline_fine_seg_block3_num158/models_2020-10-13_14-31-40/models/model_486.pt"
+torchscript_model_dir = "/fileser/zhangfan/models_zoo/fine_rib_seg_tmp/model.pt"
 
-if __name__ == "__main__":
-    model_dir = "/fileser/zhangfan/LungProject/lung_segment/experiments/rib_centerline_seg/rib_centerline_fine_seg/" \
-                "output/rib_centerline_fine_seg_block3_num158/models_2020-10-13_14-31-40/models/model_486.pt"
-    save_dir = "/fileser/zhangfan/models_zoo/fine_rib_seg_tmp/model.pt"
+model = UNet()
+if os.path.exists(pytorch_model_dir):
+    print('Loading pre_trained model...')
+    checkpoint = torch.load(pytorch_model_dir)
+    model.load_state_dict({k.replace('module.', ""): v for k, v in checkpoint['state_dict'].items()})
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
-    net_config = {"num_class": 2,
-                  "nb_filter": [12, 24, 48, 96],
-                  "input_size": [224, 160, 224],
-                  "clip_window": [-1200, 1200]}
-    model = UNet(net_config)
-    model = load_model_weights(model, model_dir)
-    model = model.cuda().half()
-    dummy_input = torch.rand(1, 1, 512, 512, 512).float().cuda().half()
-    convert_model_to_torchscript(model, dummy_input, save_dir)
+# model = model.cuda().half()
+model = model.cuda()
+for param in model.parameters():
+    param.requires_grad = False
+
+# convert torch script model.
+model.eval()
+# dummy_input = torch.rand(1, 1, 256, 192, 256).float().cuda().half()
+dummy_input = torch.rand(1, 1, 256, 192, 256).float().cuda()
+with torch.no_grad():
+    traced_script_module = torch.jit.trace(model, dummy_input)
+print(traced_script_module.code)
+traced_script_module.save(torchscript_model_dir)
